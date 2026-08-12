cda3108272f7dfe3da108631b0a59276eaf32382
Aung Ye Thant Hein <aungyethant@gmail.com>
2026-08-13 02:00:06 +0700

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int max = 0;
+        for(int pile: piles){
+            max = Math.max(pile,max);
+        }
+
+        int low = 1;
+        int high = max;
+
+        while(low <= high){
+            int mid = (low + high) / 2;
+            //Check the max hours
+            int hours = 0;
+            for(int pile: piles){
+                hours = hours + (int)Math.ceil(pile / (double)mid);
+            }
+            if(hours <= h){
+              high = mid - 1;
+              max = Math.min(max,mid);
+            } else{
+                low = mid + 1;
+            }
+        }
+
+        return max;
+    }
+}
