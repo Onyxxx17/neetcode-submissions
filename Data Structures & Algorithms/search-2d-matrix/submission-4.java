d88191a669ac19c79a6729c64968454ac4e69d54
Aung Ye Thant Hein <aungyethant@gmail.com>
2026-08-13 00:04:12 +0700

    Add: search-2d-matrix - submission-4

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-4.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        //Find the correct row first
+        int firstRow = 0;
+        int lastRow = matrix.length - 1;
+        int correctRow = 0;
+        int totalColumns = matrix[0].length - 1;
+
+        while(firstRow <= lastRow){
+            //Compare with the last integer in each row
+            correctRow= firstRow + (lastRow - firstRow) / 2;
+            if(target > matrix[correctRow][totalColumns]){
+                firstRow = correctRow + 1;
+            } else if(target < matrix[correctRow][0]){
+                lastRow = correctRow - 1;
+            } else{
+                break;
+            }
+        }
+
+        System.out.println(correctRow);
+
+        //Now find the integer in the correct row
+        int first = 0;
+        int last = totalColumns;
+
+        while(first <= last){
+            int mid = first + (last - first) / 2;
+            int currNum = matrix[correctRow][mid];
+            System.out.print(currNum);
+            if(target == currNum) {
+                return true;
+            } else if(target > currNum){
+                first = mid + 1;
+            } else{
+                last = mid - 1;
+            }
+        }
+
+        return false;
+    }
+}
