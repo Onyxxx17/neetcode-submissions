a23a342749de4c754e79938617bc97a6d87a64ea
Aung Ye Thant Hein <aungyethant@gmail.com>
2026-08-12 18:40:50 +0700

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,31 @@
+class MinStack {
+    //2 stacks, one to track minimum and one for normal operations
+    Stack<Integer> mainStack;
+    Stack<Integer> minStack;
+    public MinStack() {
+        mainStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        mainStack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        int popped = mainStack.pop();
+        if(minStack.peek() == popped){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return mainStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
