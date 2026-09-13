b194547476ddc58806384ce89a116659b1204e0e
Aung Ye Thant Hein <aungyethant@gmail.com>
2026-09-14 02:17:57 +0800

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) != ' ' && Character.isLetterOrDigit(s.charAt(i))){
+                sb.append(Character.toLowerCase(s.charAt(i)));
+            }
+        }
+
+        int l = 0;
+        int r = sb.length() - 1;
+
+        while(l <= r){
+            if(sb.charAt(l) != sb.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
